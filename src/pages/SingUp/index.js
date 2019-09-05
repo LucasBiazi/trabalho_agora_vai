59672bc84ee76f4135f3cd91a0fb969744ba5f90
LucasBiazi <54125160+LucasBiazi@users.noreply.github.com>
2019-09-05 00:39:34 -0300

    Update index.js

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,99 +1,38 @@
-import styled from "styled-components";
-import imagem from '../../assets/ba.jpg';
-import imagem2 from '../../assets/su.jpg';
+import React from 'react';
+import { CssSu } from './style';
+import { Link } from 'react-router-dom';
 
-export const CssSu = styled.div`
-*{
-    margin: 0;
-    padding:0;
+export default function Login() {
+    return (
+        <CssSu>
+            <section>
+                <form>
+                    <div className="ajuste">
+                        <label>Nome:</label>
+                    </div>
+                    <input type="text" required placeholder="Nome"></input>
+                    <div className="ajuste">
+                        <label>Senha:</label>
+                    </div>
+                    <input type="password" required placeholder="Senha"></input>
+                    <div className="ajuste">
+                        <label>Email:</label>
+                    </div>
+                    <input type="text" required placeholder="E-mail"></input>
+                    <div className="doisBtn">
+                        <Link to="../">
+                            <div className="divBotao">
+                                <p>CADASTRAR</p>
+                            </div>
+                        </Link>
+                        <Link to="../">
+                            <div className="divBotao">
+                                <p>INÍCIO</p>
+                            </div>
+                        </Link>
+                    </div>
+                </form>
+            </section>
+        </CssSu>
+    );
 }
-section{
-    display: flex;
-    justify-content: flex-end;
-    width: 100vw;
-    height: 100vh;
-    background-image: url(${imagem});
-}
-form{
-    display: flex;
-    flex-direction: column;
-    height:100vh;
-    width: 700px;
-    background: rgb(000,000,000, 45%);
-    justify-content: center;
-    align-items: center;
-    align-content:center;
-}
-.ajuste{
-    width: 670px;
-    justify-content: flex-start;
-    margin-left: 30px;
-}
-label{
-    margin-left: 10px;
-    color: white;
-    font-size: 22px;
-    padding: 10px;
-}
-input{
-    width: 600px;
-    border-radius: 10px;
-    color: black;
-    cursor: text;
-}
-.doisBtn{
-    height: 310px;
-    width: 400px;
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-    justify-content: space-around;
-}
-.divBotao{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: black;
-    border-radius: 7px;
-    height: 50px;
-    width: 150px;
-}
-a{
-    font-size: 10px;
-    color: white;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-    @media only screen and (max-width: 850px) {
-
-    section{
-        background-image: url(${imagem2});
-        height: 100%;
-        width: 100%;
-    }
-    form{
-        background: rgb(000,000,000, 15%);
-        display: flex;
-        justify-content: flex-start;
-        width: 100vw;
-        height: 100vh;  
-    }
-    .ajuste{
-        width: 390px;
-    }
-    label{
-        font-size: 30px;
-    }
-    input{
-    width: 325px;
-    border-radius: 10px;
-    color: black;
-    }
-    .doisBtn{
-        height: 470px;
-        justify-content: space-around;
-    }
-    }
-`;
-    
