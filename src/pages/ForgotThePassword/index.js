28af3f6be1b2be437c814998cbc5424d50b28f28
LucasBiazi <54125160+LucasBiazi@users.noreply.github.com>
2019-09-05 00:37:48 -0300

    Update index.js

--- src/pages/ForgotThePassword/index.js
+++ src/pages/ForgotThePassword/index.js
@@ -6,18 +6,20 @@ export default function Login() {
     return (
         <CssFTP>
             <section>
-                <div>
-
-                    <label className="lb1">Digite seu email:</label>
+                <form>
+                    <div className="divLb1">
+                        <label className="lb1">Digite seu email:</label>
+                    </div>
                     <input className="inemail" type="text" required placeholder="ex: x@gmail.com.br"></input>
                     <label className="lb2">Você vai receber um email com instruções para recuperar seu acesso.</label>
                     <button className="btnconf" type="submit">CONFIRMAR</button>
+                    <div className="espaco"></div>
                     <Link to="../">
                         <div className="divBotao">
                             <p>INÍCIO</p>
                         </div>
                     </Link>
-                </div>
+                </form>
             </section>
         </CssFTP>
     );
